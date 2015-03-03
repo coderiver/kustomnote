708a8d0a41b3ecb6a673e361bbf2427d044ce677
buschidos <sergey.snjozyk@gmail.com>
2015-03-03 17:09:53 +0200

    popups

--- site/js/common.js
+++ site/js/common.js
@@ -1,4 +1,7 @@
 head.ready(function() {
+
+    var openedPopup;
+
     if ( $('.js-slick').length ) {
         $('.js-slick').slick({
             autoplay: true,
@@ -22,4 +25,83 @@ head.ready(function() {
             });
         });
     }
+
+    var showPopup = function(popup) {
+        if ( openedPopup ) {
+            openedPopup.fadeOut(200);
+        }
+        disableScroll();
+        popup.fadeIn(200);
+        openedPopup = popup;
+    };
+
+    var hidePopup = function(popup) {
+        popup.fadeOut(200);
+        openedPopup = undefined;
+        enableScroll();
+    };
+
+    $('[data-popup]').each(function() {
+        var el = $(this);
+        el.on('click', function(event) {
+            event.preventDefault();
+            var popup = $('#' + el.data('popup'));
+            showPopup(popup);
+        });
+    });
+
+    $('.popup__close').on('click', function() {
+        var popup = $(this).parents('.popup');
+        hidePopup(popup);
+    });
+
+    $('.popup').on('click', function(event) {
+        event.preventDefault();
+        hidePopup($(this));
+    });
+
+    $('.popup__inner').on('click', function(event) {
+        event.stopPropagation();
+    });
+
+    // disable scroll
+    // left: 37, up: 38, right: 39, down: 40,
+    // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+    var keys = [37, 38, 39, 40];
+
+    function preventDefault(e) {
+      e = e || window.event;
+      if (e.preventDefault)
+          e.preventDefault();
+      e.returnValue = false;
+    }
+
+    function keydown(e) {
+        for (var i = keys.length; i--;) {
+            if (e.keyCode === keys[i]) {
+                preventDefault(e);
+                return;
+            }
+        }
+    }
+
+    function wheel(e) {
+      preventDefault(e);
+    }
+
+    function disableScroll() {
+      if (window.addEventListener) {
+          window.addEventListener('DOMMouseScroll', wheel, false);
+      }
+      window.onmousewheel = document.onmousewheel = window.ontouchmove = document.ontouchmove = wheel;
+      // document.onkeydown = keydown;
+    }
+
+    function enableScroll() {
+        if (window.removeEventListener) {
+            window.removeEventListener('DOMMouseScroll', wheel, false);
+        }
+        window.onmousewheel = document.onmousewheel = window.ontouchmove = document.ontouchmove = document.onkeydown = null;
+    }
+
 });
